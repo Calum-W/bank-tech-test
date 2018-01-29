baf7e1f4325c56b09846ebbe1b06d1b66c12efcc
Calum-W <cal7907@hotmail.co.uk>
2018-01-29 11:06:31 +0000

    Add a test that checks deposit adds the amount to the account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,10 @@ describe Account do
   it 'initialises with a balance of 0' do
     expect(account.balance).to eq 0
   end
+
+  describe "#deposit" do
+    it 'adds the amount to the account balance' do
+      expect{account.deposit(1000)}.to change{account.balance}.by(1000)
+    end
+  end
 end
