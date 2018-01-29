73caf2a3a14937d01cf148384b50d3e10a647f01
Calum-W <cal7907@hotmail.co.uk>
2018-01-29 12:55:02 +0000

    Move transaction formatting from deposit/withdraw to record_transaction

--- lib/account.rb
+++ lib/account.rb
@@ -9,12 +9,12 @@ class Account
 
   def deposit(amount)
     @balance += amount
-    transactions.unshift("#{current_time} || #{sprintf('%.2f', amount)} || || #{sprintf('%.2f', @balance)}")
+    record_transaction(amount, false)
   end
 
   def withdraw(amount)
     @balance -= amount
-    transactions.unshift("#{current_time} || || #{sprintf('%.2f', amount)} || #{sprintf('%.2f', @balance)}")
+    record_transaction(false, amount)
   end
 
   def print_statement
@@ -25,6 +25,10 @@ class Account
 
   def header
     "date || credit || debit || balance\n"
+  end
+
+  def record_transaction(deposited_amount, withdrawn_amount)
+    transactions.unshift("#{current_time} || #{sprintf('%.2f', deposited_amount) + " " if deposited_amount}|| #{sprintf('%.2f', withdrawn_amount) + " " if withdrawn_amount}|| #{sprintf('%.2f', @balance)}")
   end
 
   def current_time
