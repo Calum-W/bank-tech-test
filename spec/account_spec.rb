69d0f83ae6502e3102e1f205fff453092c35034d
Calum-W <cal7907@hotmail.co.uk>
2018-01-29 11:52:09 +0000

    Add a test that print_statement returns headers if no transactions have been made

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -12,11 +12,22 @@ describe Account do
     it 'adds the amount given to the account balance' do
       expect{account.deposit(1000)}.to change{account.balance}.by(1000)
     end
+
+    it 'adds a single transaction to the statements array' do
+      account.deposit(1000)
+      expect(account.statements).to eq []
+    end
   end
 
   describe "#withdraw" do
     it 'subtracts the amount given from the account balance' do
       expect{account.withdraw(1000)}.to change{account.balance}.by(-1000)
+    end
+  end
+
+  describe '#print_statement' do
+    it 'just prints headers if no transactions have been made' do
+      expect{ account.print_statement }.to output("date || credit || debit || balance\n").to_stdout
     end
   end
 end
